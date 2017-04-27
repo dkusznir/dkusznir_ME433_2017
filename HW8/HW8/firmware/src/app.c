9c6e86857166c46c7faa839f66be71caf98892e2
Dorian Kusznir <doriank87@gmail.com>
2017-04-27 14:49:24 -0500

    HW1 code working with Harmony.

--- HW8/HW8/firmware/src/app.c
+++ HW8/HW8/firmware/src/app.c
@@ -121,6 +121,30 @@ void APP_Initialize ( void )
     /* TODO: Initialize your application's state machine and other
      * parameters.
      */
+    
+    __builtin_disable_interrupts();
+
+    // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
+    __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
+
+    // 0 data RAM access wait states
+    BMXCONbits.BMXWSDRM = 0x0;
+
+    // enable multi vector interrupts
+    INTCONbits.MVEC = 0x1;
+
+    // disable JTAG to get pins back
+    DDPCONbits.JTAGEN = 0;
+
+    // do your TRIS and LAT commands here
+    
+    // LED output pin 
+    TRISAbits.TRISA4 = 0;
+    
+    // Button input pin
+    TRISBbits.TRISB4 = 1;
+    
+    __builtin_enable_interrupts();
 }
 
 
@@ -167,7 +191,28 @@ void APP_Tasks ( void )
             /* TODO: Handle error in application's state machine. */
             break;
         }
+        
+        while(1) 
+        {
+            // use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
+            // remember the core timer runs at half the CPU speed
+            _CP0_SET_COUNT(0);
+            while(_CP0_GET_COUNT() < 12000) //12000 in order to obtain 1KHz
+            {
+                // Button pressed
+                while(!PORTBbits.RB4)
+                {
+                    // Turn off LED
+                    LATAbits.LATA4 = 0;
+                }
+            }
+        
+            LATAINV = 0b10000;
+              
+        }
     }
+    
+    
 }
 
  
