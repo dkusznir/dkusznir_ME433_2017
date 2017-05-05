43c61b5e7fb757ec8ae4e38f108670575be0b61f
Dorian Kusznir <doriank87@gmail.com>
2017-05-05 18:58:00 -0500

    Finished HID tutorial and added IMU code from HW10.

--- HW11/HW11/firmware/src/app.c
+++ HW11/HW11/firmware/src/app.c
@@ -265,10 +265,6 @@ void APP_Initialize(void) {
  */
 
 void APP_Tasks(void) {
-    static int8_t vector = 0;
-    static uint8_t movement_length = 0;
-    int8_t dir_table[] = {-4, -4, -4, 0, 4, 4, 4, 0};
-
     /* Check the application's current state. */
     switch (appData.state) {
             /* Application's initial state. */
@@ -305,14 +301,11 @@ void APP_Tasks(void) {
         case APP_STATE_MOUSE_EMULATE:
             
             // every 50th loop, or 20 times per second
-            if (movement_length > 50) {
+
                 appData.mouseButton[0] = MOUSE_BUTTON_STATE_RELEASED;
                 appData.mouseButton[1] = MOUSE_BUTTON_STATE_RELEASED;
-                appData.xCoordinate = (int8_t) dir_table[vector & 0x07];
-                appData.yCoordinate = (int8_t) dir_table[(vector + 2) & 0x07];
-                vector++;
-                movement_length = 0;
-            }
+                appData.xCoordinate = (int8_t) 2;
+                appData.yCoordinate = (int8_t) 2;
 
             if (!appData.isMouseReportSendBusy) {
                 /* This means we can send the mouse report. The
@@ -364,7 +357,6 @@ void APP_Tasks(void) {
                             sizeof (MOUSE_REPORT));
                     appData.setIdleTimer = 0;
                 }
-                movement_length++;
             }
 
             break;
