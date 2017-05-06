bf1b2958cb7382a1a4f2530e3519267c14c8e84a
Dorian Kusznir <doriank87@gmail.com>
2017-05-06 17:46:16 -0500

    Finished HW12.

--- HW12/HelloWorld/app/src/main/java/com/example/doriankusznir/helloworld/MainActivity.java
+++ HW12/HelloWorld/app/src/main/java/com/example/doriankusznir/helloworld/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.doriankusznir.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    SeekBar myControl;
+    TextView myTextView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myControl = (SeekBar) findViewById(R.id.seek1);             // Instantiate SeekBar
+
+        myTextView = (TextView) findViewById(R.id.textView01);      // Instantiate TextView
+        myTextView.setText("Move Seek Bar to Update Values!");      // Just some random text
+
+        setMyControlListener();
+
+    }
+
+    // Function to detect when the slider is used.
+    private void setMyControlListener()
+    {
+        myControl.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+        {
+
+            int progressChanged = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+            {
+                progressChanged = progress;
+                myTextView.setText("The value is " + progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar)
+            {
+
+            }
+        });
     }
 }
+
+
